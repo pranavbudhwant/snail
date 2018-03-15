f21b7336e03317663c75397bc90651de90989783
Pranav Budhwant <prnvb15@gmail.com>
2018-03-15 23:46:28 +0530

    Documented the code.

--- numlib.h
+++ numlib.h
@@ -1,39 +1,87 @@
 #include<omp.h>
+
+/*
+new_2D_array():
+	Creates a 2D array and returns the pointer to the array.
+	Parameters:
+		size_t num_rows: number of rows in 2D array.
+		size_t num_columns: number of columns in 2D array.
+	Returns:
+		T**: double pointer to the 2D array of type T.
+*/
 template <typename T>
-T** new_2D_array(size_t rows, size_t columns){
-	T **array = new T*[rows];
-	for(size_t i=0;i<rows;i++) array[i] = new T[columns];
+T** new_2D_array(size_t num_rows, size_t num_columns){
+	T **array = new T*[num_rows];
+	for(size_t i=0;i<num_rows;i++) array[i] = new T[num_columns];
 	return array;
 }
+
+/*
+subtract():
+	Performs matrix subtraction, C = A - B.
+	Parameters:
+		T **A: 2D matrix of type T, input matrix A
+		T **B: 2D matrix of type T, input matrix B
+		T **C: 2D matrix of type T, resultant matrix of A - B
+		size_t num_rows: number of rows in input matrices.
+		size_t num_columns: number of columns in input matrices.
+	Returns:
+		void
+*/
 template <typename T>
-void subtract(T** A, T **B, T **C, size_t row, size_t col){
+void subtract(T **A, T **B, T **C, size_t num_rows, size_t num_columns){
 	int i,j;
 	#pragma omp parallel for schedule(static) private(j) shared(A,B,C)
-	for(i=0;i<row;i++){
-		for(j=0;j<col;j++)
+	for(i=0;i<num_rows;i++){
+		for(j=0;j<num_columns;j++)
 			C[i][j] = A[i][j] - B[i][j];
 	}
 }
+
+/*
+transpose():
+	Computes the transpose of given matrix.
+	Parameters:
+		T **array: 2D matrix of type T, input matrix.
+		T **trans: 2D matrix of type T, transpose of input matrix.
+		size_t num_rows: number of rows in input matrix.
+		size_t num_columns: number of columns in input matrix.
+	Returns:
+		void
+*/
 template <typename T>
-double transpose(T** array, T** trans, size_t rows, size_t cols){
+void transpose(T** array, T** trans, size_t num_rows, size_t num_columns){
 	int i,j;
-	clock_t beg = clock();
 	#pragma omp parallel for schedule(static) private(j)
-	for(i=0;i<rows;i++) 
-		for (j=0;j<cols;j++) 
-			trans[j][i] = (array[i][j]);
-	clock_t end = clock();
-	return (double(end-beg)/CLOCKS_PER_SEC);
+	for(i=0;i<num_rows;i++) 
+		for (j=0;j<num_columns;j++) 
+			trans[j][i] = array[i][j];
 }
+
+/*
+multiply():
+	Performs matrix multiplication, C = A * B.
+	Parameters:
+		T **A: 2D matrix of type T, input matrix A
+		T **B: 2D matrix of type T, input matrix B
+		T **C: 2D matrix of type T, resultant matrix of A * B
+		size_t num_rows_A: number of rows in input matrix A
+		size_t num_columns_A: number of columns in input matrix A
+		size_t num_rows_B: number of rows in input matrix B
+		size_t num_columns_B: number of columns in input matrix B
+	Returns:
+		bool: true, if matrix multiplication is possible(matrix sizes are compatible).
+			  false, if matrix multiplication is not possible(matrix sizes are incompatible).
+*/
 template <typename T>
-bool multiply(T** A, T** B, T** C, size_t row_A, size_t col_A, size_t row_B, size_t col_B){
-	if(col_A == row_B){
+bool multiply(T** A, T** B, T** C, size_t num_rows_A, size_t num_columns_A, size_t num_rows_B, size_t num_columns_B){
+	if(num_columns_A == num_rows_B){
 		int i,j,k;
 		#pragma omp parallel for private(j,k) schedule(static) shared(A,B,C)
-		for(i=0;i<row_A;i++){
-			for(j=0;j<col_B;j++){
+		for(i=0;i<num_rows_A;i++){
+			for(j=0;j<num_columns_B;j++){
 				C[i][j] = 0;
-				for(k=0;k<col_A;k++) 
+				for(k=0;k<num_columns_A;k++) 
 					C[i][j] += A[i][k]*B[k][j];
 			}
 		}
@@ -41,21 +89,36 @@ bool multiply(T** A, T** B, T** C, size_t row_A, size_t col_A, size_t row_B, siz
 	}
 	return 0;
 }
+
+/*
+gradient_descent():
+	Computes the parameter vector theta, using gradient descent.
+	Parameters:
+		T **X: Input vector, with size mxn.
+		T **Y: Output vector, with size mx1.
+		T **theta: Parameter vector, with size nx1.
+		size_t m: number of training examples.
+		size_t n: number of features.
+		double alpha: learning rate.
+		long int num_iter: number of iterations.
+	Returns:
+		void
+*/
 template <typename T>
 void gradient_descent(T** X, T** Y, T** theta, size_t m, size_t n, double alpha, long int num_iter){
-	T** hyp = new_2D_array<T>(m,1);
-	T** error = new_2D_array<T>(m,1);
-	T** err_adj = new_2D_array<T>(n,1);
-	T** final_error = new_2D_array<T>(n,1);
-	T** X_trans = new_2D_array<T>(n,m);
-	T** beta = new_2D_array<T>(1,1);
+	T** hypothesis = new_2D_array<T>(m,1); //Stores the value of hypothesis = X*theta
+	T** difference = new_2D_array<T>(m,1); //Stores the difference between hypothesis and output (hypothesis - Y)
+	T** derivative = new_2D_array<T>(n,1); //Stores the values of derivative terms( (hypothesis - Y)*X )
+	T** error = new_2D_array<T>(n,1); //Stores the final error values to be subtracted from parameter vector
+	T** X_transpose = new_2D_array<T>(n,m); //Stores the transpose of input vector X
+	T** beta = new_2D_array<T>(1,1); //Stores the constant learning rate/number of training examples(alpha/m)
 	beta[0][0] = T(alpha/m);
-	transpose(X,X_trans,m,n);
+	transpose(X,X_transpose,m,n);
 	for(long int itr=0; itr<num_iter; itr++){
-		multiply<T>(X,theta,hyp,m,n,n,1);
-		subtract<T>(hyp,Y,error,m,1);
-		multiply<T>(X_trans,error,err_adj,n,m,m,1);
-		multiply<T>(err_adj,beta,final_error,n,1,1,1);
-		subtract<T>(theta,final_error,theta,n,1);
+		multiply<T>(X,theta,hypothesis,m,n,n,1); //hypothesis = X*theta
+		subtract<T>(hypothesis,Y,difference,m,1); //difference = hypothesis - Y
+		multiply<T>(X_transpose,difference,derivative,n,m,m,1); //derivative = transpose(X)*difference
+		multiply<T>(derivative,beta,error,n,1,1,1); //error = (alpha/m) * derivative
+		subtract<T>(theta,error,theta,n,1); //theta -= error
 	}
 }
